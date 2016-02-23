e2e147ceafed14dcd54f333d3f4765711a0b4d42
jean-claude iehl <jean-claude.iehl@liris.cnrs.fr>
2016-02-23 18:09:44 +0100

    texture: doc

--- src/gKit/texture.cpp
+++ src/gKit/texture.cpp
@@ -9,11 +9,13 @@ GLuint make_texture( const int unit, const Image& im )
     if(im.data.size() == 0)
         return 0;
     
+    // cree la texture openGL
     GLuint texture;
     glGenTextures(1, &texture);
     glActiveTexture(GL_TEXTURE0 + unit);
     glBindTexture(GL_TEXTURE_2D, texture);
     
+    // choisit la representation des pixels de la texture en fonction de l'image
     GLenum data_format= 0;
     switch(im.channels)
     {
@@ -24,25 +26,53 @@ GLuint make_texture( const int unit, const Image& im )
         default: assert(0 && "invalid image format");
     }
     
+    // fixe les parametres de filtrage par defaut
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 
+    // transfere les donnees dans la texture
     glTexImage2D(GL_TEXTURE_2D, 0, 
         GL_RGBA, im.width, im.height, 0,
         data_format, GL_UNSIGNED_BYTE, &im.data.front());
     
+    // prefiltre la texture
     glGenerateMipmap(GL_TEXTURE_2D);
     return texture;
 }
+
 
 GLuint read_texture( const int unit, const char *filename )
 {
     Image im= read_image(filename);
     GLuint texture= make_texture(unit, im);
-    
     release_image(im);
     return texture;
+}
+
+
+int screenshot( const char *filename )
+{
+    // recupere le contenu de la fenetre / framebuffer par defaut
+    glFinish();
+    
+    glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
+    glReadBuffer(GL_BACK);
+    
+    // recupere les dimensions de la fenetre 
+    GLint viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    
+    // cree une image pour stocker le resultat
+    Image image= create_image(viewport[2], viewport[3], 4, make_color(0, 0, 0));
+    // transfere les pixels
+    glReadPixels(0, 0, viewport[2], viewport[3], 
+        GL_RGBA, GL_UNSIGNED_BYTE, (void *) &image.data.front());
+    
+    // ecrit l'image
+    int code= write_image(image, filename);
+    release_image(image);
+    return code;
 }
 
--- src/gKit/texture.h
+++ src/gKit/texture.h
@@ -8,7 +8,12 @@
 #include "image.h"
 
 
+//! cree une texture a partir d'une image \param im.
 GLuint make_texture( const int unit, const Image& im );
+//! cree une texture a partir d'un fichier \param filename.
 GLuint read_texture( const int unit, const char *filename );
+
+//! enregistre le contenu de la fenetre dans un fichier. 
+int screenshot( const char *filename );
 
 #endif
