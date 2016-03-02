947eef64a08a61f8c8d00b486efaf60d0a178fae
jciehl <devnull@localhost>
2016-03-02 16:43:19 +0100

    shader_kit: mode fullscreen + mode mesh

--- src/shader_kit.cpp
+++ src/shader_kit.cpp
@@ -25,7 +25,7 @@
 
 
 // program
-char program_filename[1024]= "toy.glsl";
+char program_filename[1024]= { 0 };
 GLuint program;
 
 // affichage des erreurs
@@ -33,10 +33,12 @@ std::string program_log;
 int program_area;
 bool program_failed;
 
-GLuint vao;
 
 //~ GLuint texture;
-//~ Mesh cube;
+char mesh_filename[1024]= { 0 };
+Mesh mesh;
+GLuint vao;
+unsigned int vertex_count;
 
 Orbiter camera;
 Widgets widgets;
@@ -59,6 +61,20 @@ void reload_program( )
     program_area= 1;
 }
 
+
+// cherche un fichier avec l'extension ext dans les options
+const char *option_find( const int argc, const char **argv, const char *ext )
+{
+    for(int i= 1; i < argc; i++)
+    {
+        std::string option(argv[i]);
+        if(option.rfind(ext) != std::string::npos)
+            return argv[i];
+    }
+    
+    return NULL;
+}
+
 //! compile les shaders et construit le programme + les buffers + le vertex array.
 //! renvoie -1 en cas d'erreur.
 int init( const int argc, const char **argv )
@@ -68,11 +84,33 @@ int init( const int argc, const char **argv )
     camera= make_orbiter();
     
     program= 0;
-    if(argc > 1) 
-        strcpy(program_filename, argv[1]);
-    reload_program();
+    const char *option;
+    option= option_find(argc, argv, ".glsl");
+    if(option != NULL)
+    {
+        strcpy(program_filename, option);
+        reload_program();
+    }
     
     glGenVertexArrays(1, &vao);
+    vertex_count= 3;
+    
+    option= option_find(argc, argv, ".obj");
+    if(option != NULL)
+    {
+        mesh= read_mesh(option);
+        if(mesh.positions.size() > 0)
+        {
+            strcpy(mesh_filename, option);
+            
+            vao= make_mesh_vertex_format(mesh);
+            vertex_count= (unsigned int) mesh.positions.size();
+            
+            Point pmin, pmax;
+            mesh_bounds(mesh, pmin, pmax);
+            orbiter_lookat(camera, center(pmin, pmax), distance(pmin, pmax));
+        }
+    }
     
     // nettoyage
     glUseProgram(0);
@@ -102,7 +140,7 @@ int quit( )
     // detruit les objets openGL
     //~ glDeleteTextures(1, &texture);
     glDeleteVertexArrays(1, &vao);
-    glDeleteProgram(program);
+    release_program(program);
     return 0;
 }
 
@@ -120,6 +158,8 @@ int draw( )
     // recupere les mouvements de la souris
     int mx, my;
     unsigned int mb= SDL_GetRelativeMouseState(&mx, &my);
+    int mousex, mousey;
+    SDL_GetMouseState(&mousex, &mousey);
     
     // deplace la camera
     if(mb & SDL_BUTTON(1))
@@ -137,17 +177,32 @@ int draw( )
     Transform mvp= projection * view * model;
     Transform mvpInv= make_inverse(mvp);
     Transform mv= model * view;
-    Transform mvInv= make_inverse(mv);
     
     // affiche l'objet
     if(program_failed == false)
     {
         glBindVertexArray(vao);
-        glUseProgram(program);
-        program_uniform(program, "mvpInvMatrix", mvpInv);
-        program_uniform(program, "mvMatrix", mv);
         
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glUseProgram(program);
+        program_uniform(program, "modelMatrix", model);
+        program_uniform(program, "modelInvMatrix", make_inverse(model));
+        program_uniform(program, "viewMatrix", view);
+        program_uniform(program, "viewInvMatrix", make_inverse(view));
+        program_uniform(program, "projectionMatrix", projection);
+        program_uniform(program, "projectionInvMatrix", make_inverse(projection));
+        
+        program_uniform(program, "mvpMatrix", mvp);
+        program_uniform(program, "mvpInvMatrix", mvpInv);
+        
+        program_uniform(program, "mvMatrix", mv);
+        program_uniform(program, "normalMatrix", make_normal_transform(mv));
+        
+        program_uniform(program, "viewport", make_vec2(window_width(), window_height()));
+        program_uniform(program, "time", (float) SDL_GetTicks());
+        program_uniform(program, "motion", make_vec3(mx, my, mb & SDL_BUTTON(1)));
+        program_uniform(program, "mouse", make_vec3(mousex, mousey, mb & SDL_BUTTON(1)));
+        
+        glDrawArrays(GL_TRIANGLES, 0, vertex_count);
     }
     
     if(key_state('s'))
@@ -166,10 +221,15 @@ int draw( )
     }
     else
     {
-        label(widgets, "running...", program);
-        begin_line(widgets);
-        if(edit(widgets, 128, program_filename))
-            reload_program();
+        label(widgets, "program '%s' running...", program_filename);
+        if(mesh_filename[0] != 0)
+        {
+            begin_line(widgets);
+            label(widgets, "mesh '%s', %u positions, %u texcoords, %u normals", mesh_filename, 
+                (unsigned int) mesh.positions.size(),
+                (unsigned int) mesh.texcoords.size(),
+                (unsigned int) mesh.normals.size());
+        }
     }
     end(widgets);
     
@@ -181,6 +241,12 @@ int draw( )
 
 int main( const int argc, const char **argv )
 {
+    if(argc == 1)
+    {
+        printf("usage: %s shader.glsl [mesh.obj] [texture0.png [texture1.png]]\n", argv[0]);
+        return 0;
+    }
+    
     Window w= create_window(1024, 640);
     if(w == NULL) 
         return 1;
