53a4d12b5a48bae1b510c978cc77db2000fb7f21
jean-claude iehl <jean-claude.iehl@liris.cnrs.fr>
2016-04-05 17:09:04 +0200

    draw: nettoyage + transformation model...

--- src/gKit/draw.cpp
+++ src/gKit/draw.cpp
@@ -3,21 +3,9 @@
 #include "window.h"
 
 
-void draw( Mesh& m, const Orbiter& camera )
+void draw( Mesh& m, const Transform& model, const Orbiter& camera, GLuint texture )
 {
     // recupere les transformations
-    Transform model= make_identity();
-    Transform view= orbiter_view_transform(camera);
-    Transform projection= orbiter_projection_transform(camera, window_width(), window_height(), 45);
-    
-    // affiche l'objet
-    draw(m, model, view, projection);
-}
-
-void draw( Mesh& m, const Orbiter& camera, GLuint texture )
-{
-    // recupere les transformations
-    Transform model= make_identity();
     Transform view= orbiter_view_transform(camera);
     Transform projection= orbiter_projection_transform(camera, window_width(), window_height(), 45);
     
@@ -25,3 +13,17 @@ void draw( Mesh& m, const Orbiter& camera, GLuint texture )
     draw(m, model, view, projection, texture);
 }
 
+void draw( Mesh& m, const Orbiter& camera, GLuint texture )
+{
+    draw(m, make_identity(), camera, texture);
+}
+
+void draw( Mesh& m, const Orbiter& camera )
+{
+    draw(m, make_identity(), camera, 0);
+}
+
+void draw( Mesh& m, const Transform& model, const Orbiter& camera )
+{
+    draw(m, model, camera, 0);
+}
