d48daa44b730bb1ceb63befbb946676359db40be
jean-claude iehl <jean-claude.iehl@liris.cnrs.fr>
2016-08-24 15:57:56 +0200

    nettoyage c++...

--- src/gKit/widgets.h
+++ src/gKit/widgets.h
@@ -64,6 +64,9 @@ int draw( )
 //! representation d'une interface graphique minimaliste.
 struct Widgets
 {
+    Widgets( ) : console(), px(0), py(0), focus(0), fx(0), fy(0), mb(0), mx(0), my(0), wx(0), wy(0), key(0), mod(0) {}
+    ~Widgets( ) {}
+    
     Text console;       //!< affichage des elements de l'interface.
     int px, py;         //!< placement du prochain widget
     
