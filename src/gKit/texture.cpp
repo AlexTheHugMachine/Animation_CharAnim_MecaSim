de55164a7b5903ccd8c03a19a5bcb8c321d31a32
jean-claude iehl <jean-claude.iehl@liris.cnrs.fr>
2016-08-25 14:16:28 +0200

    nettoyage c++: texture...

--- src/gKit/texture.cpp
+++ src/gKit/texture.cpp
@@ -3,11 +3,26 @@
 #include <cstdio>
 
 #include "texture.h"
+#include "image_io.h"
 
+
+int miplevels( const int width, const int height )
+{
+    int w= width;
+    int h= height;
+    int levels= 1;
+    while(w > 1 || h > 1)
+    {
+        w= std::max(1, w / 2);
+        h= std::max(1, h / 2);
+        levels= levels + 1;
+    }
+    
+    return levels;
+}
 
 GLuint make_texture( const int unit, const Image& im )
 {
-    //~ if(!im.isInit())
     if(im == Image::error())
         return 0;
 
@@ -16,18 +31,7 @@ GLuint make_texture( const int unit, const Image& im )
     glGenTextures(1, &texture);
     glActiveTexture(GL_TEXTURE0 + unit);
     glBindTexture(GL_TEXTURE_2D, texture);
-
-    // choisit la representation des pixels de la texture en fonction de l'image
-    GLenum data_format= 0;
-    switch(im.channels)
-    {
-        case 1: data_format= GL_RED; break;
-        case 2: data_format= GL_RG; break;
-        case 3: data_format= GL_RGB; break;
-        case 4: data_format= GL_RGBA; break;
-        default: assert(0 && "invalid image format");
-    }
-
+    
     // fixe les parametres de filtrage par defaut
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -37,8 +41,53 @@ GLuint make_texture( const int unit, const Image& im )
     // transfere les donnees dans la texture
     glTexImage2D(GL_TEXTURE_2D, 0,
         GL_RGBA, im.width, im.height, 0,
-        data_format, GL_UNSIGNED_BYTE, &im.data.front());
+        GL_RGBA, GL_FLOAT, im.buffer());
+    
+    // prefiltre la texture
+    glGenerateMipmap(GL_TEXTURE_2D);
+    return texture;
+}
 
+GLuint make_texture( const int unit, const ImageData& im )
+{
+    if(im.data.empty())
+        return 0;
+    
+    // cree la texture openGL
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glActiveTexture(GL_TEXTURE0 + unit);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    
+    // fixe les parametres de filtrage par defaut
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+    GLenum format;
+    switch(im.channels)
+    {
+        case 1: format= GL_RED; break;
+        case 2: format= GL_RG; break;
+        case 3: format= GL_RGB; break;
+        case 4: format= GL_RGBA; break;
+        default: format= GL_RGBA; 
+    }
+    
+    GLenum type;
+    switch(im.size)
+    {
+        case 1: type= GL_UNSIGNED_BYTE; break;
+        case 4: type= GL_FLOAT; break;
+        default: type= GL_UNSIGNED_BYTE;
+    }
+    
+    // transfere les donnees dans la texture
+    glTexImage2D(GL_TEXTURE_2D, 0,
+        GL_RGBA, im.width, im.height, 0,
+        format, type, &im.data.front());
+    
     // prefiltre la texture
     glGenerateMipmap(GL_TEXTURE_2D);
     return texture;
@@ -47,10 +96,8 @@ GLuint make_texture( const int unit, const Image& im )
 
 GLuint read_texture( const int unit, const char *filename )
 {
-    Image im= read_image(filename);
-    GLuint texture= make_texture(unit, im);
-    //~ release_image(im);
-    return texture;
+    ImageData image= read_image_data(filename);
+    return make_texture(unit, image);
 }
 
 
@@ -66,17 +113,13 @@ int screenshot( const char *filename )
     GLint viewport[4];
     glGetIntegerv(GL_VIEWPORT, viewport);
 
-    // cree une image pour stocker le resultat
-    //~ Image image= create_image(viewport[2], viewport[3], 4, make_color(0, 0, 0));
-    Image image(viewport[2], viewport[3], 4, make_black());
     // transfere les pixels
-    glReadPixels(0, 0, viewport[2], viewport[3],
+    ImageData image(viewport[2], viewport[3], 4);
+    glReadPixels(0, 0, image.width, image.height,
         GL_RGBA, GL_UNSIGNED_BYTE, &image.data.front());
 
     // ecrit l'image
-    int code= write_image(image, filename);
-    //~ release_image(image);
-    return code;
+    return write_image_data(image, filename);
 }
 
 int capture( const char *prefix )
--- src/gKit/texture.h
+++ src/gKit/texture.h
@@ -19,6 +19,9 @@ GLuint make_texture( const int unit, const Image& im );
 //! cree une texture a partir d'un fichier filename. a detruire avec glDeleteTextures( ).
 GLuint read_texture( const int unit, const char *filename );
 
+//! renvoie le nombre de mipmap d'une image width x height.
+int miplevels( const int width, const int height );
+
 //! enregistre le contenu de la fenetre dans un fichier filename nom de l'image a ecrire. doit etre de type .png / .bmp
 int screenshot( const char *filename );
 
