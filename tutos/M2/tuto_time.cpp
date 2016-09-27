96ea77542f337a4723ab7e8405cbe030552901c4
jean-claude iehl <jean-claude.iehl@liris.cnrs.fr>
2016-09-27 15:25:28 +0200

    tuto_time + doc...

--- tutos/M2/tuto_time.cpp
+++ tutos/M2/tuto_time.cpp
@@ -23,6 +23,7 @@ public:
     
     int init( )
     {
+        // charge un objet et une texture a afficher
         m_objet= read_mesh("data/bigguy.obj");        
         Point pmin, pmax;
         m_objet.bounds(pmin, pmax);
@@ -30,11 +31,11 @@ public:
 
         m_texture= read_texture(0, "data/debug2x2red.png");
 
-        // affichage du temps de glDraw
-        m_console= create_text();
-
         // mesure du temps gpu de glDraw
         glGenQueries(1, &m_time_query);
+
+        // affichage du temps  dans la fenetre
+        m_console= create_text();
 
         // etat openGL par defaut
         glClearColor(0.2f, 0.2f, 0.2f, 1.f);        // couleur par defaut de la fenetre
@@ -49,8 +50,8 @@ public:
     int quit( )
     {
         glDeleteQueries(1, &m_time_query);
-        release_text(m_console);
         
+        release_text(m_console);
         m_objet.release();
         glDeleteTextures(1, &m_texture);
         
@@ -79,6 +80,7 @@ public:
         
         // mesure le temps d'execution du draw pour le gpu
         glBeginQuery(GL_TIME_ELAPSED, m_time_query);
+        
         // mesure le temps d'execution du draw pour le cpu
         // utilise std::chrono pour mesurer le temps cpu 
         std::chrono::high_resolution_clock::time_point cpu_start= std::chrono::high_resolution_clock::now();
@@ -121,8 +123,12 @@ public:
         printf(m_console, 0, 1, "gpu  %02dms %03dus", (int) (gpu_time / 1000000), (int) ((gpu_time / 1000) % 1000));
         printf(m_console, 0, 2, "wait %02dms %03dus", (int) (wait_time / 1000000), (int) ((wait_time / 1000) % 1000));
         
-        // affiche le texte
+        // affiche le texte dans la fenetre de l'application, utilise console.h
         draw(m_console, window_width(), window_height());
+
+        // affiche le temps dans le terminal 
+        printf("cpu  %02dms %03dus    ", (int) (cpu_time / 1000000), (int) ((cpu_time / 1000) % 1000));
+        printf("gpu  %02dms %03dus\n", (int) (gpu_time / 1000000), (int) ((gpu_time / 1000) % 1000));
         
         return 1;
     }
