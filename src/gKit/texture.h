e39b9a293843645fba7fd7a7e693da133e05e0cd
jean-claude iehl <jean-claude.iehl@liris.cnrs.fr>
2016-10-12 14:09:11 +0200

    doc + tuto framebuffer...

--- src/gKit/texture.h
+++ src/gKit/texture.h
@@ -14,13 +14,16 @@
 //! texture2D openGL.
 
 
-//! cree une texture a partir d'une image im. a detruire avec glDeleteTextures( ).
+//! cree une texture a partir d'une image im. a detruire avec glDeleteTextures( ). 
+//! \param texel_type permet de choisir la representation interne des valeurs de la texture.
 GLuint make_texture( const int unit, const Image& im, const GLenum texel_type= GL_RGBA32F );
 
 //! cree une texture a partir des donnees d'une image, cf image_io.h. a detruire avec glDeleteTextures( ).
+//! \param texel_type permet de choisir la representation interne des valeurs de la texture.
 GLuint make_texture( const int unit, const ImageData& im, const GLenum texel_type= GL_RGBA );
 
 //! cree une texture a partir d'un fichier filename. a detruire avec glDeleteTextures( ).
+//! \param texel_type permet de choisir la representation interne des valeurs de la texture.
 GLuint read_texture( const int unit, const char *filename, const GLenum texel_type= GL_RGBA );
 
 //! renvoie le nombre de mipmap d'une image width x height.
