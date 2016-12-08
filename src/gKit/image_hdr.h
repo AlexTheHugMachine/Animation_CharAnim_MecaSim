55223e82b643069834f62298e9a6876580b5d4d0
jean-claude iehl <jean-claude.iehl@liris.cnrs.fr>
2016-12-08 12:56:10 +0100

    image: format hdr...

---
+++ src/gKit/image_hdr.h
@@ -0,0 +1,23 @@
+
+#ifndef _IMAGE_HDR_H
+#define _IMAGE_HDR_H
+
+#include "image.h"
+
+
+//! \addtogroup image utilitaires pour manipuler des images
+//@{
+
+//! \file
+//! manipulation directe d'images, format .hdr
+
+//! charge une image a partir d'un fichier .hdr. renvoie Image::error() en cas d'echec. a detruire avec image::release( ).
+//! \param filemane nom de l'image .hdr a charger
+Image read_image_hdr( const char *filename );
+
+//! enregistre une image dans un fichier .hdr.
+int write_image_hdr( const Image& image, const char *filename );
+
+//@}
+
+#endif
