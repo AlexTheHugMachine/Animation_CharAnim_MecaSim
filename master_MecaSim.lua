9becc701f8286045f47b00b0d1475e782345625e
Florence Zara <devnull@localhost>
2017-01-12 17:39:57 +0100

    Modif pour compil master_MecaSim

--- master_MecaSim.lua
+++ master_MecaSim.lua
@@ -2,17 +2,17 @@
 no_project = true
 dofile "./premake4.lua"
 
--- master_CharAnim
+-- master_MecaSim
 gfx_masterMecaSim_dir = path.getabsolute(".")
 
-master_CharAnim_files = {	gfx_masterMecaSim_dir .. "/src/master_MecaSim/*.cpp", 
-							gfx_masterMecaSim_dir .. "/src/master_MecaSim/*.h"
+master_MecaSim_files = {	gfx_masterMecaSim_dir .. "/src/master_MecaSim/src-etudiant/*.cpp", 
+							gfx_masterMecaSim_dir .. "/src/master_MecaSim/src-etudiant/*.h"
 	}
 	
-project("master_MecaSim")
+project("master_MecaSim_etudiant")
     language "C++"
     kind "ConsoleApp"
     targetdir ( gfx_masterMecaSim_dir .. "/bin" )
-	includedirs { gfx_masterMecaSim_dir .. "/src/master_MecaSim/" }
+	includedirs { gfx_masterMecaSim_dir .. "/src/master_MecaSim/src-etudiant/" }
     files ( gkit_files )
-    files ( master_CharAnim_files )
+    files ( master_MecaSim_files )
