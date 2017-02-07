9281d6e4633ab41c6c9ba1a22479020fa588351a
Florence Zara <devnull@localhost>
2017-02-07 09:03:14 +0100

    mis les noms des fichiers

--- l2_lifgfx.lua
+++ l2_lifgfx.lua
@@ -3,8 +3,14 @@ no_project = true
 dofile "./premake4.lua"
 
 l2_gfx_dir = path.getabsolute(".")
-
-l2_lifgfx_files = {	l2_gfx_dir .. "/src/l2_lifgfx/*", l2_gfx_dir .. "/src/l2_lifgfx/pacman_core/*" }
+	
+l2_lifgfx_files = {	l2_gfx_dir .. "/src/l2_lifgfx/AnimationCurve.cpp", 
+				l2_gfx_dir .. "/src/l2_lifgfx/AnimationCurve.h",
+				l2_gfx_dir .. "/src/l2_lifgfx/Viewer.cpp",
+				l2_gfx_dir .. "/src/l2_lifgfx/Viewer.h",
+				l2_gfx_dir .. "/src/l2_lifgfx/main.cpp",
+				l2_gfx_dir .. "/src/l2_lifgfx/pacman_core/*"}	
+	
 	
 project("l2_lifgfx")
     language "C++"
