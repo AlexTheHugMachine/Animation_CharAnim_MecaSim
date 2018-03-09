4f9775896a9714cee7e903c5a9fadac27404d497
Vincent Nivoliers <vincent.nivoliers@gmail.com>
2018-03-10 00:13:28 +0100

    option for output dir and allow memory growth

--- emscripten/emscripten_build.sh
+++ emscripten/emscripten_build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+target=$1
+output="bin/$1"
+opts=`getopt -o o: --long output: -n 'parse-options' -- "$@"`
+
+if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+
+eval set -- "$opts"
+
+while true; do
+  case "$1" in
+    -o | --output ) output=$2; shift; shift ;;
+    * ) break ;;
+  esac
+done
+
+echo "output is $output"
+
 export CC=emcc
 export CXX=em++
 export PATH=/usr/lib/emscripten:$PATH
@@ -8,8 +25,6 @@ flags="-s USE_SDL=2 -s USE_SDL_IMAGE=2 --use-preload-plugins"
 
 export CXXFLAGS=$flags
 
-target=$1
-
 emmake make $target
-mv "bin/$target" "bin/$target.bc"
-emcc $flags -O3 "./bin/$target.bc" -o ."/bin/$target.html" --preload-file data --shell-file emscripten/shell_minimal.html
+mv "$output" "$output.bc"
+emcc $flags -O3 -s ALLOW_MEMORY_GROWTH=1 "$output.bc" -o "$output.html" --preload-file data --shell-file emscripten/shell_minimal.html
