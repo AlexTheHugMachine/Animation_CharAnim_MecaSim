2a6c3da066c129ee9525122eafcdcdd7356aea2f
jean-claude.iehl <jean-claude.iehl@univ-lyon1.fr>
2021-06-21 15:18:24 +0200

    tuto alpha: hashed alpha testing

--- tutos/alpha.cpp
+++ tutos/alpha.cpp
@@ -1,4 +1,11 @@
 
+// "hashed alpha testing" https://casual-effects.com/research/Wyman2017Hashed/
+
+// \todo utiliser la quantification du bruit pour le stabiliser...
+
+// \todo comparer avec "Weighted Blended Order-Independent Transparency" http://jcgt.org/published/0002/02/09/
+
+
 #include "wavefront.h"
 
 #include "draw.h"
@@ -60,14 +67,20 @@ public:
         // decrire un repere / grille 
         m_repere= make_grid(20);
         
+        Point grid_pmin, grid_pmax;
+        m_repere.bounds(grid_pmin, grid_pmax);
+        
+        //~ m_objet= read_mesh("data/alpha.obj");   // utiliser alpha.glsl !!
+        m_objet= read_mesh("data/robot.obj");   // utiliser alpha_notexture.glsl !!
+        
         Point pmin, pmax;
-        m_repere.bounds(pmin, pmax);
+        m_objet.bounds(pmin, pmax);
+        
+        pmin= min(pmin, grid_pmin);
+        pmax= max(pmax, grid_pmax);
+
         // parametrer la camera de l'application, renvoyee par la fonction camera()
         camera().lookat(pmin, pmax);
-        
-        m_objet= read_mesh("data/alpha.obj");
-        //~ m_objet= read_mesh("data/robot.obj");
-        m_vao= m_objet.create_buffers( /* texcoords */ true, /* normals */ true, /* color */ false, /* material index */ false);
         
         // charge les textures, si necessaire
         Materials& materials= m_objet.materials();
@@ -75,7 +88,24 @@ public:
         for(unsigned i= 0; i < m_textures.size(); i++)
             m_textures[i]= read_texture(0, materials.filename(i));
         
-        m_program= read_program("tutos/alpha.glsl");
+        // affiche les matieres
+        {
+            for(int i= 0; i < materials.count(); i++)
+            {
+                const Material& material= materials.material(i);
+                printf("material[%d] '%s' kd %f %f %f ", i, materials.name(i), material.diffuse.r, material.diffuse.g, material.diffuse.b);
+                if(material.diffuse_texture != -1)
+                    printf("texture '%s'", materials.filename(material.diffuse_texture));
+                printf("\n");
+            }
+        }
+        
+        m_groups= m_objet.groups();
+        m_vao= m_objet.create_buffers( /* texcoords */ true, /* normals */ true, /* color */ false, /* material index */ false);
+        
+        //
+        //~ m_program= read_program("tutos/alpha.glsl");
+        m_program= read_program("tutos/alpha_notexture.glsl");
         program_print_errors(m_program);
         
         // etat openGL par defaut
@@ -85,12 +115,8 @@ public:
         glDepthFunc(GL_LESS);                       // ztest, conserver l'intersection la plus proche de la camera
         glEnable(GL_DEPTH_TEST);                    // activer le ztest
         
-        if(window_msaa() > 1)
-        {
-            glEnable(GL_MULTISAMPLE);
-            glEnable(GL_SAMPLE_ALPHA_TO_COVERAGE);
-            //~ glEnable(GL_SAMPLE_ALPHA_TO_ONE);
-        }
+        glEnable(GL_MULTISAMPLE);   // MSAA
+        glEnable(GL_SAMPLE_ALPHA_TO_COVERAGE);  // transformer la transparence en visibilite des samples MSAA
         
         return 0;   // ras, pas d'erreur
     }
@@ -108,11 +134,15 @@ public:
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
+        // dessine aussi le repere, pour le meme point de vue
+        draw(m_repere, Identity(), camera());
+        
         if(key_state('r'))
         {
             // recharge le shader a la volee...
             clear_key_state('r');
-            reload_program(m_program, "tutos/alpha.glsl");
+            //~ reload_program(m_program, "tutos/alpha.glsl");
+            reload_program(m_program, "tutos/alpha_notexture.glsl");
             program_print_errors(m_program);
         }
         
@@ -127,13 +157,28 @@ public:
         
         program_uniform(m_program, "mvMatrix", mv);
         program_uniform(m_program, "mvpMatrix", mvp);
-        if(m_textures.size())
-            program_use_texture(m_program, "alpha", 0, m_textures[0]);
-            
-        glDrawArrays(GL_TRIANGLES, 0, m_objet.vertex_count());
         
-        // dessine aussi le repere, pour le meme point de vue
-        draw(m_repere, Identity(), camera());
+        // dessine tous les groupes de triangles, tries par matiere
+        const Materials& materials= m_objet.materials();
+        for(unsigned i= 0; i < m_groups.size(); i++)
+        {
+            const Material& material= materials.material(m_groups[i].material_index);
+            if(material.diffuse_texture != -1)
+                program_use_texture(m_program, "alpha", 0, m_textures[material.diffuse_texture]);
+            else
+                program_use_texture(m_program, "alpha", 0, 0);
+            
+            //~ printf("group[%u] material %d first %d n %d, texture %d\n", i, m_groups[i].material_index, m_groups[i].first, m_groups[i].n, material.diffuse_texture);
+            
+            glDrawArrays(GL_TRIANGLES, m_groups[i].first, m_groups[i].n);
+        }
+        
+        if(key_state('s'))
+        {
+            clear_key_state('s');
+            static int id= 1;
+            screenshot("alpha", id++);
+        }
         
         // continuer...
         return 1;
@@ -145,6 +190,7 @@ protected:
     GLuint m_vao;
     GLuint m_program;
     std::vector<GLuint> m_textures;
+    std::vector<TriangleGroup> m_groups;
 };
 
 
