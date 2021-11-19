8e9ee81f19ae2711c14b7de4a6cee0387a13dad0
jean-claude.iehl <jean-claude.iehl@univ-lyon1.fr>
2021-11-19 15:55:18 +0100

    M2: tuto_read_buffer et count_buffer, manipulation shared atomics...

---
+++ tutos/M2/read_buffer.glsl
@@ -0,0 +1,24 @@
+
+#version 430
+
+#ifdef COMPUTE_SHADER
+
+layout( std430, binding= 0 ) buffer inputData
+{
+	int a[];
+};
+
+layout( std430, binding= 1 ) buffer outputData
+{
+	int b[];
+};
+
+layout( local_size_x=256 ) in;
+void main( )
+{
+	uint ID= gl_GlobalInvocationID.x;
+	if(ID < a.length())
+		b[ID]= a[ID] + 10;
+}
+
+#endif
